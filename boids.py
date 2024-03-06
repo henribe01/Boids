0aaafbc0e4af6b31eccb779c032ae957d9eddcac
Henri Benecke <13785517+henribe01@users.noreply.github.com>
2024-03-06 21:17:59 +0100

    Add flocking behavior to boids

--- boids.py
+++ boids.py
@@ -5,17 +5,20 @@ class Boid:
     def __init__(self, pos: np.ndarray, vel: np.ndarray, WIDTH: int, HEIGHT: int):
         self.pos = pos
         self.vel = vel
-        self.acc = np.zeros(2)
         self.artist = None
         self.WIDTH = WIDTH
         self.HEIGHT = HEIGHT
+
+        self.cohesion_weight = 1
+        self.alignment_weight = 1
+        self.separation_weight = 1
 
     def plot(self, ax: plt.Axes) -> plt.Artist:
         """
         Plots the boid as an triangle pointing in the direction of the velocity on the given axis.
         """
         # TODO: For now, I just plot the position of the boid as a point. Replace this with a triangle.
-        self.artist, = ax.plot(self.pos[0], self.pos[1], 'ro')
+        self.artist, = ax.plot(self.pos[0], self.pos[1], 'ro', markersize=3)
 
     def update_plot(self) -> plt.Artist:
         """
@@ -25,14 +28,31 @@ class Boid:
         self.artist.set_data(self.pos[0], self.pos[1])
         return self.artist
 
-    def update(self) -> None:
+    def update(self, neighborhood: list, max_speed: float = 4, max_force: float = 1) -> None:
         """
         Updates the position and velocity of the boid based on its current acceleration.
         """
-        self.pos += self.vel
         self.check_bounds(self.WIDTH, self.HEIGHT)
-        self.vel += self.acc
-        self.acc = np.zeros(2)
+
+        alignment = self.align(neighborhood)
+        if np.linalg.norm(alignment) > max_force:
+            alignment = max_force * alignment / np.linalg.norm(alignment)
+        
+        cohesion = self.cohesion(neighborhood)
+        if np.linalg.norm(cohesion) > max_force:
+            cohesion = max_force * cohesion / np.linalg.norm(cohesion)
+
+        separation = self.separation(neighborhood)
+        if np.linalg.norm(separation) > max_force:
+            separation = max_force * separation / np.linalg.norm(separation)
+
+        # Update the velocity
+        acc = alignment * self.alignment_weight + cohesion * self.cohesion_weight + separation * self.separation_weight
+        self.vel += acc
+        if np.linalg.norm(self.vel) > max_speed:
+            self.vel = max_speed * self.vel / np.linalg.norm(self.vel)
+        self.pos += self.vel
+
 
     def check_bounds(self, width: int, height: int) -> None:
         """
@@ -47,12 +67,53 @@ class Boid:
         elif self.pos[1] > height:
             self.pos[1] = 0
 
-    def _cohesion(self, boids: list) -> np.ndarray:
+    def get_neighborhood(self, boids: list, radius: float) -> list:
         """
-        Returns the average position of the boids in the given list and returns the vector pointing towards it.
+        Returns a list of boids within the given radius of this boid
         """
-        center = np.zeros(2)
+        # TODO: Add angle to the condition
+        neighborhood = []
         for boid in boids:
-            if boid != self:
-                center += boid.pos
-        return center / (len(boids) - 1) - self.pos
+            width_vec = [self.WIDTH, 0]
+            height_vec = [0, self.HEIGHT]
+            distances = [
+                np.linalg.norm(boid.pos - self.pos),
+                np.linalg.norm(boid.pos - (self.pos + width_vec)),
+                np.linalg.norm(boid.pos - (self.pos + height_vec)),
+                np.linalg.norm(boid.pos - (self.pos - width_vec)),
+                np.linalg.norm(boid.pos - (self.pos - height_vec))
+            ]
+            if min(distances) < radius and boid != self:
+                neighborhood.append(boid)
+        return neighborhood
+    
+    def align(self, neighborhood: list) -> np.ndarray:
+        """
+        Returns the alignment vector of the boid based on the velocities of its neighbors.
+        """
+        if len(neighborhood) == 0:
+            return np.zeros(2)
+        avg_vel = np.mean([boid.vel for boid in neighborhood], axis=0)
+        return avg_vel - self.vel
+
+    def cohesion(self, neighborhood: list) -> np.ndarray:
+        """
+        Returns the cohesion vector of the boid based on the positions of its neighbors.
+        """
+        if len(neighborhood) == 0:
+            return np.zeros(2)
+        avg_pos = np.mean([boid.pos for boid in neighborhood], axis=0)
+        #TODO: Set magnitude to max_speed
+        return avg_pos - self.pos - self.vel
+    
+    def separation(self, neighborhood: list) -> np.ndarray:
+        """
+        Returns the separation vector of the boid based on the positions of its neighbors.
+        """
+        if len(neighborhood) == 0:
+            return np.zeros(2)
+        separation = np.zeros(2)
+        for boid in neighborhood:
+            diff = self.pos - boid.pos
+            separation += diff / np.linalg.norm(diff)*
+        return separation / len(neighborhood) - self.vel
--- simulation.py
+++ simulation.py
@@ -3,29 +3,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-WIDTH = 100
-HEIGHT = 100
-NUM_BOIDS = 10
+WIDTH = 50
+HEIGHT = 50
+NUM_BOIDS = 20
 
 
-fig, ax = plt.subplots()
-ax.set_xlim(0, WIDTH)
-ax.set_ylim(0, HEIGHT)
-boids = []
-for _ in range(NUM_BOIDS):
-    pos = np.random.rand(2) * [WIDTH, HEIGHT]
-    vel = np.random.uniform(-1, 1, 2)
-    b = Boid(pos, vel, WIDTH, HEIGHT)
-    b.plot(ax)
-    boids.append(b)
+class Flock:
+    def __init__(self, num_boids: int, width: int, height: int, radius: float) -> None:
+        # Initialize the boids
+        self.boids = []
+        for _ in range(num_boids):
+            pos = np.random.rand(2) * [width, height]
+            vel = np.random.uniform(-1, 1, 2)
+            b = Boid(pos, vel, width, height)
+            self.boids.append(b)
 
-def update(frame: int) -> None:
-    updated_artists = []
-    for boid in boids:
-        boid.update()
-        artist = boid.update_plot()
-        updated_artists.append(artist)
-    return updated_artists
+        self.radius = radius
 
-ani = FuncAnimation(fig, update, blit=True, interval=1000/100, save_count=50)
-plt.show()    
+        # Create the plot
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_xlim(0, width)
+        self.ax.set_ylim(0, height)
+        self.artists = [b.plot(self.ax) for b in self.boids]
+        self.ani = FuncAnimation(self.fig, self.update, blit=True, interval=1000/60, save_count=50)
+
+    def show(self) -> None:
+        plt.show()
+
+    def update(self, frame: int) -> list:
+        updated_artists = []
+        for boid in self.boids:
+            neighborhood = boid.get_neighborhood(self.boids, self.radius)
+            boid.update(neighborhood)
+            artist = boid.update_plot()
+            updated_artists.append(artist)
+        return updated_artists
+
+
+if __name__ == "__main__":
+    f = Flock(NUM_BOIDS, WIDTH, HEIGHT, 10)
+    f.show()
